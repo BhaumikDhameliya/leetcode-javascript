831afa4c0cb50083d8c8ffbcc04e17015275c0ad
Bhaumik Dhameliya <bhaumikdhameliya30@gmail.com>
2023-05-29 09:32:18 +0530

    2618. Check if Object Instance of Class

---
+++ 2618-Check-if-Object-Instance-of-Class.js
@@ -0,0 +1,19 @@
+/**
+ * @param {any} obj
+ * @param {any} classFunction
+ * @return {boolean}
+ */
+var checkIfInstanceOf = function (obj, classFunction) {
+  if (obj === null || obj === undefined || typeof classFunction !== "function")
+    return false;
+  let curProto = Object.getPrototypeOf(obj);
+  while (curProto !== null) {
+    if (curProto === classFunction.prototype) return true;
+    curProto = Object.getPrototypeOf(curProto);
+  }
+  return false;
+};
+
+/**
+ * checkIfInstanceOf(new Date(), Date); // true
+ */
