3b88016cc7c32e81f3b3641cd426a35654fb2019
BhaumikDhameliya <bhaumikdhameliya30@gmail.com>
2024-09-21 13:53:12 +0530

    386. Lexicographical Numbers

--- 386-Lexicographical-Numbers.js
+++ 386-Lexicographical-Numbers.js
@@ -17,3 +17,20 @@ var lexicalOrder = function (n) {
   }
   return res;
 };
+
+var lexicalOrder2 = function (n) {
+  const res = [];
+  let cur = 1;
+  while (res.length < n) {
+    res.push(cur);
+    if (cur * 10 <= n) {
+      cur *= 10;
+    } else {
+      while (cur == n || cur % 10 == 9) {
+        cur = Math.floor(cur / 10);
+      }
+      cur += 1;
+    }
+  }
+  return res;
+};
