58ef795ab780465354cc73a81085991f0ba42e9e
BhaumikDhameliya <bhaumikdhameliya30@gmail.com>
2024-10-15 22:55:10 +0530

    2938. Separate Black and White Balls

---
+++ 2938-Separate-Black-and-White-Balls.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var minimumSteps = function (s) {
+  const s_arr = s.split("");
+  let res = 0;
+  let zero_count = 0;
+  for (let i = 0; i < s_arr.length; i++) {
+    if (s_arr[i] === "0") zero_count++;
+  }
+  for (let i = 0; i < zero_count; i++) {
+    if (s_arr[i] === "1") {
+      for (let j = zero_count; j < s_arr.length; j++) {
+        if (s_arr[j] === "0") {
+          res += j - i;
+          s_arr[j] = "1";
+          s_arr[i] = "0";
+          zero_count = j; // starting point after swap
+          break;
+        }
+      }
+    }
+  }
+  return res;
+};
